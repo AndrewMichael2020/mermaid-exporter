613dbe9c36a3683cf66ff4ded37fe8c79224ae57
A M <andriy.ignatov@gmail.com>
2025-11-22 06:12:05 +0000

    it's not layout; it's the container here would not start

--- next.config.js
+++ next.config.js
@@ -7,6 +7,28 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
