b22dc50e471925883bf44ee7be350da604f36567
Andrew Michael <63167427+AndrewMichael2020@users.noreply.github.com>
2025-11-28 09:40:19 +0000

    chore(eslint): remove redundant configuration from .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,3 @@ module.exports = {
     // project-specific overrides
   },
 };
-module.exports = {
-  root: true,
-  extends: ['next/core-web-vitals'],
-  rules: {
-    // project-specific overrides
-  },
-};
