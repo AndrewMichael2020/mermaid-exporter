a2b5058867e26a7c3b35c2c3ede33d8bf7fdba94
Andrew Michael <63167427+AndrewMichael2020@users.noreply.github.com>
2025-11-28 09:13:48 +0000

    chore(eslint): use FlatCompat for next/core-web-vitals in flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
 module.exports = [
+  ...compat.extends("next/core-web-vitals"),
   {
-    extends: ["next/core-web-vitals"],
     rules: {
       // project-specific rule overrides can go here
-    }
-  }
+    },
+  },
 ];
 
